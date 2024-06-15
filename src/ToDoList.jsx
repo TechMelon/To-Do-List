bca7d02d069e1a23aba6b978f2f2ec8471f41b1d
sneha <snehabhardwaj035@gmail.com>
2024-06-15 18:02:08 +0530

    first commit

---
+++ src/ToDoList.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+function ToDoList() {
+
+    const [tasks, setTasks] = useState([]);
+    const [newTasks, setNewTasks] = useState("");
+
+    function handlezinputChange(event) {
+        
+    }
+    function addTask() {
+        
+    }
+    function deleteTasks(index) {
+        
+    }
+    function moveTaskUp(index) {
+        
+    }
+    function moveTaskDown(index) {
+        
+    }
+
+    return(
+        <div></div>
+    );
+}
+export default ToDoList
